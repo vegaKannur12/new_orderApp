693fcecb31dd54c9cd1143b5469a93d06ef1d7b9
vegaKannur12 <100991705+vegaKannur12@users.noreply.github.com>
2022-07-19 10:41:38 +0530

    collection cancel success

--- lib/components/customPopup.dart
+++ lib/components/customPopup.dart
@@ -4,8 +4,17 @@ import 'package:orderapp/db_helper.dart';
 import 'package:provider/provider.dart';
 
 class CustomPopup {
+  String? gen_condition;
+
   Widget buildPopupDialog(BuildContext context, String content, String type,
       int rowNum, String userId, String date, String aid) {
+    String? gen_area =
+        Provider.of<Controller>(context, listen: false).areaidFrompopup;
+    if (gen_area != null) {
+      gen_condition = " and accountHeadsTable.area_id=$gen_area";
+    } else {
+      gen_condition = " ";
+    }
     return new AlertDialog(
       // title: const Text('Popup example'),
       content: new Column(
@@ -23,7 +32,9 @@ class CustomPopup {
               await OrderAppDB.instance.upadteCommonQuery(
                   "collectionTable", "rec_cancel='1'", "rec_row_num=$rowNum");
               Provider.of<Controller>(context, listen: false)
-                  .dashboardSummery(userId, date, aid,context);
+                  .dashboardSummery(userId, date, aid, context);
+              Provider.of<Controller>(context, listen: false)
+                  .todayCollection(date, gen_condition!);
             }
             Navigator.of(context).pop();
           },
